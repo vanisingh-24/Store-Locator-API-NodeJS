772ab68f4b791ee59c3c97ee155fe58cb4b8d9a8
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-10-24 16:42:07 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+//Load env variables
+dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
+
+const app = express();
+
+//Body Parser Middleware
+app.use(express.json());
+
+//Enable Cors
+app.use(cors());
+
+//Routes
+app.use('/api/v1/stores', require('./routes/stores'));
+
+app.get('/api/v1/stores',(req,res)=> {
+    res.send('Hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
---
+++ models/Store.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
+
+const StoreSchema = new mongoose.Schema({
+    storeId: {
+        type: String,
+        required: [true, 'Please add a store ID'],
+        unique: true,
+        trim: true,
+        maxlength: [10, 'Store ID must be less than 10 characters']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        type: {
+          type: String, 
+          enum: ['Point'], 
+        },
+        coordinates: {
+          type: [Number],
+          index: '2dsphere'
+        },
+        formattedAddress: String
+      },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//Geocode & create location
+StoreSchema.pre('save', async function(next){
+    const loc = await geocoder.geocode(this.address);
+    console.log(loc);
+});
+
+module.exports = mongoose.model('Store', StoreSchema);
